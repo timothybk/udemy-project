00730f60e7f75cbc9dd1cfdd9a4f3f937f4c7230
Timothy BucklandKerr <timbk86@gmail.com>
2017-07-24 08:12:53 +0000

    at 13

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Output() optionClicked = new EventEmitter<string>()
+  
 
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  onSelected(clickData: string){
+    this.optionClicked.emit(clickData)
   }
 
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,10 +7,10 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-	recipes: Recipe[] = [
+    recipes: Recipe[] = [
 		new Recipe('a test', 'this is just a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
 		new Recipe('a test', 'this is just a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
-	];	
+	];
 
   constructor() { }
 
